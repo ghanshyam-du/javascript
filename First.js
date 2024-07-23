c7cea67aa5da0f00f438439567e7da2ce1bbfda8
ghanshyam-du <135561990+ghanshyam-du@users.noreply.github.com>
2024-07-23 19:53:07 +0000

    Pending changes exported from your codespace

---
+++ First.js
@@ -0,0 +1,69 @@
+
+// this is the way we are creating the object in javascript
+
+// const student = {
+//     fullName : "Rahul kumar",
+//     age : 20,
+//     cpga : 6.9,
+//     isPass : true
+// };
+
+// student["age"] = student["age"] + 1;
+
+// console.log(student.age);
+
+
+// const product = {
+//    name : "Parker  jotter Standard CT Ball Pen",
+//    rating : 4,
+//    price : 270,
+//    offer : 5,
+//    isDeal : true
+// };
+
+// console.log(typeof product);
+
+
+// const profile = {
+//     name : "Ghanshyam Dubey",
+//     Follow : true,
+//     verified : true,
+//     posts : 195,
+//     followers : 569,
+//     following : 4,
+//     role : "Enterprenure"
+// };
+
+// // console.log(typeof profile["name"]);
+
+// let a = 7;
+// let b = "7";
+// console.log(a===b);
+// 9+8
+//  let color;
+//  if(mode === "dark-mode"){
+//     color = "black";
+//  }
+
+// let age = 4;
+// if(age > 18)
+// {
+//     console.log("you can vote");
+// }
+// else{
+//     console.log("You cannot vate");
+// }
+
+// let mode = "light";
+// let color;
+// if(mode === "dark")
+// {
+//     color = "black";
+// }
+// else
+// {
+//     color = "white";
+// }
+// console.log(color);
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # javascript
+ let,const & var
+  let- variable can be re-declared & updated. A global scope variable.
+
+
+
+Operators in js :- used to perform some operation on data
+
+=== this will check the value and it's type 

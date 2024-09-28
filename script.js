2764202c3e9e52f1266af209ee238ab8e247844d
ghanshyam-du <135561990+ghanshyam-du@users.noreply.github.com>
2024-09-28 10:01:47 +0000

    Pending changes exported from your codespace

---
+++ script.js
@@ -0,0 +1,11 @@
+ var arr = [1,2,3,4,5,6];
+//  arr.splice(2,1);
+// arr.unshift(4);
+// arr.unshift(0);
+arr.shift();
+
+//  for(var i = 0; i< arr.length; i++){
+//     console.log(arr);
+//  }
+console.log(arr);
+ 

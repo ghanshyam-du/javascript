639ab7f883f6d5216fed8ebf457d833c59583b27
ghanshyam-du <135561990+ghanshyam-du@users.noreply.github.com>
2024-10-08 09:48:02 +0000

    data type

---
+++ 01_basics/l2.js
@@ -0,0 +1,18 @@
+/* 
+Lecture- 2
+*/
+
+"use strict"; // treat all js code as newer version
+// alert("hello")  we are using nodejs not browser
+
+let name = "ghanshyam" //string => ""
+let age = 20  //number => 2 to power 53
+let isLoggedIn = false // boolean => true/false
+
+// null => standalone value
+// undefined = value is not assgined up to now
+// symbol => unique
+
+console.log(typeof undefined); // throw undefine 
+console.log(typeof null);  // throw object
+
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,22 @@
-console.log("ghanshyam")
+/*
+Lecture- 1
+*/
+const accountId = 144553
+let accountEmail = "ghanshyamdu@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mundra"
+let accountState; // it is undefine 
+
+// accountId = 2 changing the things which is decleared by the const will not allowed to change. It will throw the error.
+accountEmail = "12alhae@gmail.com"
+accountPassword = "21212121"
+accountCity = "jaypur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+// ----------------------------------------------------

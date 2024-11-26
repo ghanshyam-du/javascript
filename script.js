0f959f7395c89639ee4926a959ed243401debe11
ghanshyam-du <135561990+ghanshyam-du@users.noreply.github.com>
2024-11-26 10:25:14 +0000

    js

--- script.js
+++ script.js
@@ -1,11 +1,109 @@
- var arr = [1,2,3,4,5,6];
-//  arr.splice(2,1);
-// arr.unshift(4);
-// arr.unshift(0);
-arr.shift();
+// let a = prompt("Enter day")
+// a = Number.parseInt(a)
+// switch(a){
+//     case 1:
+//         alert("monday")
+//         break
+//     case 2:
+//         alert("tuesday")
+//         break
+//     case 3:
+//         alert("wednesday")
+//         break
+//     default:
+//         alert("invalid day")            
+// }
+// alert("you can" + (a<18 ? "not drive" : "drive"))
+// if(a>10 && a<20)
+// {
+//     alert("the age is lies between 10 and 20")
+// }
+// else{
+//     alert("not lies in between 10 and 20")
+// }
+// if(a % 2== 0 && a % 3 == 0)
+// {
+//     alert("the given number is divisible by 2 and 3")
+// }
+// else{
+//     alert("it is not at all divisible ")
+// }
+// if(a % 2 == 0)
+// {
+//     alert("the given number is divisible by 2")
+// }
+// else if(a % 3 == 0)
+// {
+//     alert("the given number is divisible by 3")
+// }
+// else{
+//     alert("the given number is not divisible by nor 2 and 3")
+// }
 
-//  for(var i = 0; i< arr.length; i++){
-//     console.log(arr);
-//  }
-console.log(arr);
- 
+// alert("you can" + (a<18? "not drive" : "drive"))
+
+
+// let sum = 0
+// let n = prompt("Enter the value of n: ")
+// n = Number.parseInt(n)
+
+// for(let i = 1; i < n ; i++)
+// {
+//     sum += i
+// }
+// alert("the sum of the n natural number is : " + sum)
+// ------------------------ for in loop example 
+// let obj = {
+//      harry: 90,
+//      shubh: 45,
+//      shivika: 56,
+//      ritika: 24,
+//      shiv: 78
+// }
+// for(let a in obj)
+// {
+//     alert("marks of "+ a + "are" + obj[a])
+// }
+
+// -------------------------- for of loop example
+// for(let b of "ghanshyam")    // here object should be iterative 
+// {
+//     alert(b)
+// }
+
+
+// ---------------------------------------- while loop
+
+// let a = prompt("entar the value of a: ")
+// a = Number.parseInt(a)
+
+// // while(a < 10){
+// //     alert(a)
+// //     a++
+// // }
+// let i = 0
+// do{
+//     alert(i)
+//     i++
+// }while(i<a)
+
+// const hello = ()=>{
+//     console.log("hay how are you. i am toh fine yaar")
+//     return "hi"
+// }
+// hello()
+
+
+// function onePlusAvg(a, b)
+// {
+//     return (a + b) / 2
+// }
+
+// let p = onePlusAvg(4,5)
+// console.log(p)
+
+
+// let firstName = `ghan
+// akjads a
+// a a `
+// console.log(firstName)
